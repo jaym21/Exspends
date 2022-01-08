f182a6b5d70118754c44391693adc40a790d8510
jaym21 <jaynmistry21@gmail.com>
2022-01-08 10:27:48 +0530

    added dependency injection to provide app database

---
+++ app/src/main/java/dev/jaym21/exspends/di/AppModule.kt
@@ -0,0 +1,22 @@
+package dev.jaym21.exspends.di
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.jaym21.exspends.db.AppDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(application: Application): AppDatabase =
+        Room.databaseBuilder(application, AppDatabase::class.java, "app_database")
+            .build()
+
+}
