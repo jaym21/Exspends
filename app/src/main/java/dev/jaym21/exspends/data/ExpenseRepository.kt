c111b5268c64828e2706cad26c3e4e0f2beca9f8
jaym21 <jaynmistry21@gmail.com>
2022-01-08 18:54:00 +0530

    added functions to make database operations in repository

--- app/src/main/java/dev/jaym21/exspends/data/ExpenseRepository.kt
+++ app/src/main/java/dev/jaym21/exspends/data/ExpenseRepository.kt
@@ -1,9 +1,19 @@
 package dev.jaym21.exspends.data
 
 import dev.jaym21.exspends.data.db.AppDatabase
+import dev.jaym21.exspends.data.models.Expense
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ExpenseRepository @Inject constructor(private val database: AppDatabase) {
 
+    suspend fun insertExpense(expense: Expense) = database.getExpenseDAO().insertExpense(expense)
 
+    suspend fun deleteExpense(expense: Expense) = database.getExpenseDAO().deleteExpense(expense)
+
+    suspend fun updateExpense(expense: Expense) = database.getExpenseDAO().updateExpense(expense)
+
+    fun getAllTransaction(): Flow<List<Expense>> = database.getExpenseDAO().getAllExpenses()
+
+    fun getExpensesByCategory(category: String): Flow<List<Expense>> = database.getExpenseDAO().getExpensesByCategory(category)
 }
