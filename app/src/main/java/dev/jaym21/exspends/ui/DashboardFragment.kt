b46e3c94278be1a8084edd800b5b44a993e6abae
jaym21 <jaynmistry21@gmail.com>
2022-01-08 21:09:59 +0530

    added navigation from dashboard to add expense fragment

--- app/src/main/java/dev/jaym21/exspends/ui/DashboardFragment.kt
+++ app/src/main/java/dev/jaym21/exspends/ui/DashboardFragment.kt
@@ -5,15 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import dev.jaym21.exspends.R
+import dev.jaym21.exspends.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding: FragmentDashboardBinding
+        get() = _binding!!
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //initializing navController
+        navController = Navigation.findNavController(view)
+
+        binding.fabAddExpense.setOnClickListener {
+            navController.navigate(R.id.action_dashboardFragment_to_addExpenseFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
