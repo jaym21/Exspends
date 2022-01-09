032e3441354aeea6924f958405dd343881c942cf
jaym21 <jaynmistry21@gmail.com>
2022-01-09 14:08:18 +0530

    added add, remove and update expense functions in expense viewModel

---
+++ app/src/main/java/dev/jaym21/exspends/ui/ExpenseViewModel.kt
@@ -0,0 +1,26 @@
+package dev.jaym21.exspends.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.jaym21.exspends.data.ExpenseRepository
+import dev.jaym21.exspends.data.models.Expense
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ExpenseViewModel @Inject constructor(private val repo: ExpenseRepository): ViewModel() {
+
+    fun addExpense(expense: Expense) = viewModelScope.launch(Dispatchers.IO) {
+        repo.insertExpense(expense)
+    }
+
+    fun removeExpense(expense: Expense) = viewModelScope.launch(Dispatchers.IO) {
+        repo.deleteExpense(expense)
+    }
+
+    fun updateExpense(expense: Expense) = viewModelScope.launch(Dispatchers.IO) {
+        repo.updateExpense(expense)
+    }
+}
