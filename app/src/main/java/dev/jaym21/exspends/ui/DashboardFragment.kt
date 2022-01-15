023f2eb4d25d09fb72d464b823b6fa9d7739d88c
jaym21 <jaynmistry21@gmail.com>
2022-01-15 09:54:48 +0530

    implemented displaying total expenses

--- app/src/main/java/dev/jaym21/exspends/ui/DashboardFragment.kt
+++ app/src/main/java/dev/jaym21/exspends/ui/DashboardFragment.kt
@@ -1,7 +1,6 @@
 package dev.jaym21.exspends.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -27,6 +26,7 @@ class DashboardFragment : Fragment() {
     private lateinit var navController: NavController
     private var expensesAdapter = ExpensesRVAdapter()
     private lateinit var viewModel: ExpenseViewModel
+    private var totalExpenses = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -59,12 +59,17 @@ class DashboardFragment : Fragment() {
                 when(it) {
                     is ExpenseState.Success -> {
                         binding.progressBar.visibility = View.GONE
-                        val expenses = if (it.expenses.size > 10) {
+                        val latestExpenses = if (it.expenses.size > 10) {
                             it.expenses.subList(0, 10)
                         } else {
                             it.expenses
                         }
-                        expensesAdapter.submitList(expenses)
+                        expensesAdapter.submitList(latestExpenses)
+
+                        it.expenses.forEach { expense ->
+                            totalExpenses += expense.amount
+                        }
+                        binding.tvTotalExpenses.text = "₹$totalExpenses"
                     }
                     is ExpenseState.Loading -> {
                         binding.progressBar.visibility = View.VISIBLE
