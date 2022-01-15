4b60108abbb7273635f1729776837eb6e6de87c6
jaym21 <jaynmistry21@gmail.com>
2022-01-15 11:04:32 +0530

    implementing pie chart

--- app/src/main/java/dev/jaym21/exspends/ui/DashboardFragment.kt
+++ app/src/main/java/dev/jaym21/exspends/ui/DashboardFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import dagger.hilt.android.AndroidEntryPoint
 import dev.jaym21.exspends.R
 import dev.jaym21.exspends.adapters.ExpensesRVAdapter
@@ -81,17 +87,61 @@ class DashboardFragment : Fragment() {
                             totalExpenses += expense.amount
                         }
                         binding.tvTotalExpenses.text = "₹$totalExpenses"
+
                         updateCategoryTotal(it.expenses)
+                        setUpPieChart()
                     }
                     is ExpenseState.Loading -> {
                         binding.progressBar.visibility = View.VISIBLE
                     }
                     is ExpenseState.Empty -> {
                         binding.progressBar.visibility = View.GONE
+                        binding.pieChart.visibility = View.GONE
                     }
                 }
             }
         }
+    }
+
+    private fun setUpPieChart() {
+        binding.pieChart.description.isEnabled = false
+        binding.pieChart.isDrawHoleEnabled = true
+        binding.pieChart.holeRadius = 58f
+        binding.pieChart.setUsePercentValues(true)
+        binding.pieChart.setDrawEntryLabels(false)
+        binding.pieChart.setDrawCenterText(true)
+        binding.pieChart.highlightValues(null)
+        binding.pieChart.setHoleColor(ContextCompat.getColor(requireContext(), R.color.bg_color))
+        binding.pieChart.animateY(1400, Easing.EaseInOutQuad)
+
+
+        val entries = ArrayList<PieEntry>()
+        entries.add(PieEntry((totalGroceries * 100/totalExpenses).toFloat(), "Groceries"))
+        entries.add(PieEntry((totalShopping * 100/totalExpenses).toFloat(), "Shopping"))
+        entries.add(PieEntry((totalSubscriptions * 100/totalExpenses).toFloat(), "Subscriptions"))
+        entries.add(PieEntry((totalEntertainment * 100/totalExpenses).toFloat(), "Entertainment"))
+        entries.add(PieEntry((totalRestaurant * 100/totalExpenses).toFloat(), "Restaurant"))
+        entries.add(PieEntry((totalTravel * 100/totalExpenses).toFloat(), "Travel"))
+        entries.add(PieEntry((totalBills * 100/totalExpenses).toFloat(), "Bills"))
+        entries.add(PieEntry((totalOthers * 100/totalExpenses).toFloat(), "Others"))
+
+        val colors = ArrayList<Int>()
+        colors.add(ContextCompat.getColor(requireContext(), R.color.green))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.blue))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.orange))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.red))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.purple))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.yellow))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.turquoise))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.grey))
+
+        val dataSet = PieDataSet(entries, "")
+        dataSet.colors = colors
+
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter())
+        binding.pieChart.data = data
+        binding.pieChart.invalidate()
     }
 
     private fun updateCategoryTotal(expenses: List<Expense>) {
