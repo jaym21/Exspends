2a65a37709e10a1c10c381735d250d13d8c72661
jaym21 <jaynmistry21@gmail.com>
2022-01-24 13:39:04 +0530

    added abstract function to monthly expense dao in database

--- app/src/main/java/dev/jaym21/exspends/data/db/AppDatabase.kt
+++ app/src/main/java/dev/jaym21/exspends/data/db/AppDatabase.kt
@@ -3,9 +3,12 @@ package dev.jaym21.exspends.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import dev.jaym21.exspends.data.models.Expense
+import dev.jaym21.exspends.data.models.MonthlyExpense
 
-@Database(entities = [Expense::class], version = 1, exportSchema = false)
+@Database(entities = [Expense::class, MonthlyExpense::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
 
     abstract fun getExpenseDAO(): ExpenseDAO
+
+    abstract fun getMonthlyExpenseDAO(): MonthlyExpenseDAO
 }
