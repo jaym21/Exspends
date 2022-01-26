c24514588296a5b5a98133d0fe4b776305322667
jaym21 <jaynmistry21@gmail.com>
2022-01-26 19:49:48 +0530

    added current month and monthly exspends fragments

---
+++ app/src/main/java/dev/jaym21/exspends/ui/charts/MonthlyExspendsFragment.kt
@@ -0,0 +1,19 @@
+package dev.jaym21.exspends.ui.charts
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.exspends.R
+
+class MonthlyExspendsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_monthly_exspends, container, false)
+    }
+}
