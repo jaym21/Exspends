6aebaa8d47c5ebebb62f4c1561af8de8818d9a4c
jaym21 <jaynmistry21@gmail.com>
2022-01-28 23:36:47 +0530

    added fragment to show all monthly exspends

---
+++ app/src/main/java/dev/jaym21/exspends/ui/charts/AllMonthlyExspendsFragment.kt
@@ -0,0 +1,34 @@
+package dev.jaym21.exspends.ui.charts
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.exspends.R
+import dev.jaym21.exspends.databinding.FragmentAllMonthlyExspendsBinding
+
+class AllMonthlyExspendsFragment : Fragment() {
+
+    private var _binding: FragmentAllMonthlyExspendsBinding? = null
+    private val binding: FragmentAllMonthlyExspendsBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentAllMonthlyExspendsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
