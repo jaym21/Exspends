2b158bde3075b10ada313be5ed01db9777468c20
jaym21 <jaynmistry21@gmail.com>
2022-01-30 10:55:36 +0530

    implementing update expense fragment functionality

--- app/src/main/java/dev/jaym21/exspends/ui/ExpenseUpdateFragment.kt
+++ app/src/main/java/dev/jaym21/exspends/ui/ExpenseUpdateFragment.kt
@@ -1,21 +1,37 @@
 package dev.jaym21.exspends.ui
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import dev.jaym21.exspends.R
+import dev.jaym21.exspends.data.models.Expense
 import dev.jaym21.exspends.databinding.FragmentExpenseUpdateBinding
+import dev.jaym21.exspends.utils.DateConverterUtils
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ExpenseUpdateFragment : Fragment() {
+@AndroidEntryPoint
+class ExpenseUpdateFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentExpenseUpdateBinding? = null
     private val binding: FragmentExpenseUpdateBinding
         get() = _binding!!
     private lateinit var navController: NavController
     private lateinit var viewModel: ExpenseViewModel
+    private var expense: Expense? = null
+    private var expenseTimestamp: Long? = null
+    private var categorySelected: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +45,239 @@ class ExpenseUpdateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //initializing navController
+        navController = Navigation.findNavController(view)
+
+        expense = arguments?.getSerializable("expense") as Expense
+
+        viewModel = ViewModelProvider(this).get(ExpenseViewModel::class.java)
+
+        binding.ivBack.setOnClickListener {
+            navController.popBackStack()
+        }
+
+        if (expense != null) {
+
+            initializeOnClick()
+
+            binding.etExpenseDateUpdate.setOnClickListener {
+                openDatePicker()
+            }
+
+            updateCategoryUnderlineAndIcon(expense!!.category)
+
+            binding.tvCategoryName.text = "${expense!!.category[0].uppercase()}${expense!!.category.substring(1)}"
+            binding.etExpenseTitleUpdate.setText(expense!!.title)
+            binding.etExpenseAmountUpdate.setText("₹${expense!!.amount}")
+            binding.etExpenseDateUpdate.setText(expense!!.date)
+        } else {
+            navController.popBackStack()
+        }
+
+    }
+
+    private fun updateCategoryUnderlineAndIcon(category:  String) {
+        when(category) {
+            "groceries" -> {
+                binding.underline.background = ContextCompat.getDrawable(requireContext(), R.drawable.groceries_underline_bg)
+                Glide.with(requireContext()).load(R.drawable.ic_groceries).into(binding.ivCategoryIcon)
+                binding.ivCategoryIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            }
+            "shopping" -> {
+                binding.underline.background = ContextCompat.getDrawable(requireContext(), R.drawable.shopping_underline_bg)
+                Glide.with(requireContext()).load(R.drawable.ic_shopping).into(binding.ivCategoryIcon)
+                binding.ivCategoryIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.blue))
+            }
+            "subscription" -> {
+                binding.underline.background = ContextCompat.getDrawable(requireContext(), R.drawable.subscriptions_underline_bg)
+                Glide.with(requireContext()).load(R.drawable.ic_subscriptions).into(binding.ivCategoryIcon)
+                binding.ivCategoryIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.orange))
+            }
+            "entertainment" -> {
+                binding.underline.background = ContextCompat.getDrawable(requireContext(), R.drawable.entertainment_underline_bg)
+                Glide.with(requireContext()).load(R.drawable.ic_entertainment).into(binding.ivCategoryIcon)
+                binding.ivCategoryIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.red))
+            }
+            "restaurant" -> {
+                binding.underline.background = ContextCompat.getDrawable(requireContext(), R.drawable.restaurant_underline_bg)
+                Glide.with(requireContext()).load(R.drawable.ic_restaurant).into(binding.ivCategoryIcon)
+                binding.ivCategoryIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.purple))
+            }
+            "travel" -> {
+                binding.underline.background = ContextCompat.getDrawable(requireContext(), R.drawable.travel_underline_bg)
+                Glide.with(requireContext()).load(R.drawable.ic_travel).into(binding.ivCategoryIcon)
+                binding.ivCategoryIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.yellow))
+            }
+            "bills" -> {
+                binding.underline.background = ContextCompat.getDrawable(requireContext(), R.drawable.bills_underline_bg)
+                Glide.with(requireContext()).load(R.drawable.ic_bill).into(binding.ivCategoryIcon)
+                binding.ivCategoryIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.turquoise))
+            }
+            "others" -> {
+                binding.underline.background = ContextCompat.getDrawable(requireContext(), R.drawable.others_underline_bg)
+                Glide.with(requireContext()).load(R.drawable.ic_others).into(binding.ivCategoryIcon)
+                binding.ivCategoryIcon.setColorFilter(ContextCompat.getColor(requireContext(), R.color.grey))
+            }
+        }
+    }
+
+    private fun openDatePicker() {
+        val calendar = Calendar.getInstance()
+        val dateToday = Date()
+
+        val datePickerOnDateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, day ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, day)
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            binding.etExpenseDateUpdate.setText(sdf.format(calendar.time))
+        }
+
+        DatePickerDialog(
+            requireContext(),
+            datePickerOnDateSetListener,
+            calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+        ).run {
+            datePicker.minDate = DateConverterUtils.getFirstDayOfMonthTimestamp()
+            datePicker.maxDate = dateToday.time
+            show()
+        }
+    }
+
+    //adding category onClick listeners
+    private fun initializeOnClick() {
+        binding.tvGroceries.setOnClickListener(this)
+        binding.tvShopping.setOnClickListener(this)
+        binding.tvSubscription.setOnClickListener(this)
+        binding.tvEntertainment.setOnClickListener(this)
+        binding.tvRestaurant.setOnClickListener(this)
+        binding.tvTravel.setOnClickListener(this)
+        binding.tvBills.setOnClickListener(this)
+        binding.tvOthers.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.tvGroceries -> {
+                categorySelected = "groceries"
+                binding.tvGroceries.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_groceries_selected_bg)
+                binding.tvShopping.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_shopping_notselected_bg)
+                binding.tvSubscription.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_subscription_notselected_bg)
+                binding.tvEntertainment.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_entertainment_notselected_bg)
+                binding.tvRestaurant.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_restaurant_notselected_bg)
+                binding.tvTravel.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_travel_notselected_bg)
+                binding.tvBills.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_personal_notselected_bg)
+                binding.tvOthers.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_others_notselected_bg)
+            }
+            R.id.tvShopping -> {
+                categorySelected = "shopping"
+                binding.tvGroceries.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_groceries_notselected_bg)
+                binding.tvShopping.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_shopping_selected_bg)
+                binding.tvSubscription.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_subscription_notselected_bg)
+                binding.tvEntertainment.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_entertainment_notselected_bg)
+                binding.tvRestaurant.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_restaurant_notselected_bg)
+                binding.tvTravel.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_travel_notselected_bg)
+                binding.tvBills.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_personal_notselected_bg)
+                binding.tvOthers.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_others_notselected_bg)
+            }
+            R.id.tvSubscription -> {
+                categorySelected = "subscription"
+                binding.tvGroceries.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_groceries_notselected_bg)
+                binding.tvShopping.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_shopping_notselected_bg)
+                binding.tvSubscription.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_subscription_selected_bg)
+                binding.tvEntertainment.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_entertainment_notselected_bg)
+                binding.tvRestaurant.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_restaurant_notselected_bg)
+                binding.tvTravel.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_travel_notselected_bg)
+                binding.tvBills.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_personal_notselected_bg)
+                binding.tvOthers.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_others_notselected_bg)
+            }
+            R.id.tvEntertainment -> {
+                categorySelected = "entertainment"
+                binding.tvGroceries.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_groceries_notselected_bg)
+                binding.tvShopping.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_shopping_notselected_bg)
+                binding.tvSubscription.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_subscription_notselected_bg)
+                binding.tvEntertainment.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_entertainment_selected_bg)
+                binding.tvRestaurant.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_restaurant_notselected_bg)
+                binding.tvTravel.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_travel_notselected_bg)
+                binding.tvBills.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_personal_notselected_bg)
+                binding.tvOthers.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_others_notselected_bg)
+            }
+            R.id.tvRestaurant -> {
+                categorySelected = "restaurant"
+                binding.tvGroceries.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_groceries_notselected_bg)
+                binding.tvShopping.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_shopping_notselected_bg)
+                binding.tvSubscription.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_subscription_notselected_bg)
+                binding.tvEntertainment.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_entertainment_notselected_bg)
+                binding.tvRestaurant.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_restaurant_selected_bg)
+                binding.tvTravel.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_travel_notselected_bg)
+                binding.tvBills.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_personal_notselected_bg)
+                binding.tvOthers.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_others_notselected_bg)
+            }
+            R.id.tvTravel -> {
+                categorySelected = "travel"
+                binding.tvGroceries.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_groceries_notselected_bg)
+                binding.tvShopping.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_shopping_notselected_bg)
+                binding.tvSubscription.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_subscription_notselected_bg)
+                binding.tvEntertainment.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_entertainment_notselected_bg)
+                binding.tvRestaurant.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_restaurant_notselected_bg)
+                binding.tvTravel.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_travel_selected_bg)
+                binding.tvBills.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_personal_notselected_bg)
+                binding.tvOthers.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_others_notselected_bg)
+            }
+            R.id.tvBills -> {
+                categorySelected = "bills"
+                binding.tvGroceries.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_groceries_notselected_bg)
+                binding.tvShopping.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_shopping_notselected_bg)
+                binding.tvSubscription.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_subscription_notselected_bg)
+                binding.tvEntertainment.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_entertainment_notselected_bg)
+                binding.tvRestaurant.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_restaurant_notselected_bg)
+                binding.tvTravel.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_travel_notselected_bg)
+                binding.tvBills.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_personal_selected_bg)
+                binding.tvOthers.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_others_notselected_bg)
+            }
+            R.id.tvOthers -> {
+                categorySelected = "others"
+                binding.tvGroceries.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_groceries_notselected_bg)
+                binding.tvShopping.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_shopping_notselected_bg)
+                binding.tvSubscription.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_subscription_notselected_bg)
+                binding.tvEntertainment.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_entertainment_notselected_bg)
+                binding.tvRestaurant.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_restaurant_notselected_bg)
+                binding.tvTravel.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_travel_notselected_bg)
+                binding.tvBills.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_personal_notselected_bg)
+                binding.tvOthers.background = ContextCompat.getDrawable(requireContext(), R.drawable.category_others_selected_bg)
+            }
+        }
+    }
+
+    private fun checkAllFieldsEntered(): Boolean {
+        if (binding.etExpenseTitleUpdate.text.isNotEmpty() && binding.etExpenseTitleUpdate.text.isNotBlank()) {
+            if (binding.etExpenseAmountUpdate.text.isNotEmpty() && binding.etExpenseAmountUpdate.text.isNotBlank()) {
+                if (binding.etExpenseDateUpdate.text.isNotEmpty() && binding.etExpenseDateUpdate.text.isNotBlank()) {
+                    if (categorySelected != null) {
+                        return true
+                    }else {
+                        Snackbar.make(binding.root, "Expense category needs to be selected", Snackbar.LENGTH_SHORT).show()
+                    }
+                }else {
+                    Snackbar.make(binding.root, "Expense date cannot be empty", Snackbar.LENGTH_SHORT).show()
+                }
+            }else {
+                Snackbar.make(binding.root, "Expense amount cannot be empty", Snackbar.LENGTH_SHORT).show()
+            }
+        } else {
+            Snackbar.make(binding.root, "Expense title cannot be empty", Snackbar.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        animateUnderline()
+    }
+
+    private fun animateUnderline() {
+        val animation = AnimationUtils.loadAnimation(requireContext(), R.anim.underline_animation)
+        binding.underline.startAnimation(animation)
     }
 
     override fun onDestroy() {
