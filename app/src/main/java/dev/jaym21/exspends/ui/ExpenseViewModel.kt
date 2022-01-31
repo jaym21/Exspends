e8ece532042f0792db5eace2f19970fd682cea32
jaym21 <jaynmistry21@gmail.com>
2022-01-31 12:04:06 +0530

    removed individual category count from expense viewmodel

--- app/src/main/java/dev/jaym21/exspends/ui/ExpenseViewModel.kt
+++ app/src/main/java/dev/jaym21/exspends/ui/ExpenseViewModel.kt
@@ -20,15 +20,6 @@ class ExpenseViewModel @Inject constructor(private val repo: ExpenseRepository):
     val allExpenses: StateFlow<AllExpensesState> = _allExpenses
 
     var totalExpenses = 0.0
-    var totalGroceries = 0.0
-    var totalShopping = 0.0
-    var totalSubscriptions = 0.0
-    var totalEntertainment = 0.0
-    var totalRestaurant = 0.0
-    var totalTravel = 0.0
-    var totalBills = 0.0
-    var totalInvestments = 0.0
-    var totalOthers = 0.0
 
     fun addExpense(expense: Expense) = viewModelScope.launch(Dispatchers.IO) {
         repo.insertExpense(expense)
@@ -49,19 +40,9 @@ class ExpenseViewModel @Inject constructor(private val repo: ExpenseRepository):
             } else {
                 //resetting the totals
                 totalExpenses = 0.0
-                totalGroceries = 0.0
-                totalShopping = 0.0
-                totalSubscriptions = 0.0
-                totalEntertainment = 0.0
-                totalRestaurant = 0.0
-                totalTravel = 0.0
-                totalBills = 0.0
-                totalInvestments = 0.0
-                totalOthers = 0.0
 
-                //updating the total expense and total for categories
+                //updating the total expenses
                 updateTotalExpenses(expenses)
-                updateCategoryTotal(expenses)
 
                 _allExpenses.value = AllExpensesState.Success(expenses)
             }
@@ -71,40 +52,6 @@ class ExpenseViewModel @Inject constructor(private val repo: ExpenseRepository):
     private fun updateTotalExpenses(expenses: List<Expense>) {
         expenses.forEach {
             totalExpenses += it.amount
-        }
-    }
-
-    private fun updateCategoryTotal(expenses: List<Expense>) {
-        expenses.forEach { expense ->
-            when(expense.category) {
-                "groceries" -> {
-                    totalGroceries += expense.amount
-                }
-                "shopping" -> {
-                    totalShopping += expense.amount
-                }
-                "subscription" -> {
-                    totalSubscriptions += expense.amount
-                }
-                "entertainment" -> {
-                    totalEntertainment += expense.amount
-                }
-                "restaurant" -> {
-                    totalRestaurant += expense.amount
-                }
-                "travel" -> {
-                    totalTravel += expense.amount
-                }
-                "bills" -> {
-                    totalBills += expense.amount
-                }
-                "investments" -> {
-                    totalInvestments += expense.amount
-                }
-                "others" -> {
-                    totalOthers += expense.amount
-                }
-            }
         }
     }
 }
